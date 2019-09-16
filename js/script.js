80b44fa66791e04bfbf44f5720afd87c2265fc98
Laura Garfias <lauragarfias07@gmail.com>
2019-09-15 20:37:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,48 +3,24 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-let studentList = document.getElementsByTagName('li')
-const pageItems =  10
+//global variables
+let studentList = document.getElementsByTagName('li');
+const pageItems =  10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+//Hide all of the items in the list except for the ten I want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 const showPage = (list, page) => {
    const startIndex = (page * parseInt(pageItems)) - parseInt(pageItems)
    const endIndex = page * parseInt(pageItems)
-   if(let i = 0; i >= startIndex && i <= endIndex; i ++) {
-      document.write(studentList[i])
+   for(let i = 0; i >= startIndex && i <= endIndex; i ++) {
+      return studentList[i];
    }
 }
+showPage(studentList, 1);
+//Generate, append, and add functionality to the pagination buttons.
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+/***
+const appendPageLinks = (list) => {
+
+}
 ***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
