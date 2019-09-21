a525f8e994f9da2a4317352d0c7d369b95733194
Laura Garfias <lauragarfias07@gmail.com>
2019-09-20 21:49:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,26 +3,54 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-//global variables
+//Global variables
 let studentList = document.getElementsByTagName('li');
 const pageItems =  10;
 
 //Hide all of the items in the list except for the ten I want to show.
 
 const showPage = (list, page) => {
-   const startIndex = (page * pageItems) - pageItems
-   const endIndex = page * pageItems
+   const endIndex = (page * pageItems) - 1;
+   const startIndex = endIndex - 9;
    for(let i = 0; i <= studentList.length; i ++) {
       if(i >= startIndex && i <= endIndex){
-         return studentList[i];
+         document.write(studentList[i]);
       }
    }
 }
-console.log(showPage(studentList, 1));
+
 //Generate, append, and add functionality to the pagination buttons.
 
-/***
 const appendPageLinks = (list) => {
+   const parentDiv = document.querySelector('.page');
+   const div = document.createElement('div');
+   div.className = 'pagination';
+   parentDiv.appendChild(div);
+   const ul = document.createElement('ul');
+   div.appendChild(ul);
+   const page = list.length/pageItems;
 
+   for(let i = 0; i < page; i ++) {
+      const li = document.createElement('li');
+      ul.appendChild(li);
+      const link = document.createElement('a');
+      link.href = '#';
+      const text = i + 1
+      link.textContent = text
+      li.appendChild(link);
+      if(link.textContent === 1) {
+         link.className = 'active';
+      }
+      link.addEventListener('click', (e) => {
+         const allLinks = document.getElementsByTagName('a');
+         const clicked = event.target;
+         for(let i = 0; i < allLinks.length; i ++) {
+            link.textContent[i].classList.remove('active');
+         }
+         clicked.className = 'active';
+         showPage(studentList, link.textContent);
+      })
+   }
 }
-***/
+
+appendPageLinks(studentList);
